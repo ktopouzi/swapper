74310aee5a62693139ec5053aa43650cbe5b3a39
Kimon Topouzidis <kimon.topouzidis@gmail.com>
2020-10-23 14:15:09 +0100

    adds tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        "sw-yellow": "#ffeb3b"
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+};
